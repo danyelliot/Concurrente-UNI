4a60821baca3a3e3723d4dbb5167d51ed65e72ad
DagmarLV <dagmar.lezama.v@uni.pe>
2023-09-28 22:59:28 -0500

    server menu edits

--- PC2/server/src/main/java/com/concurrente/server/server/HelloController.java
+++ PC2/server/src/main/java/com/concurrente/server/server/HelloController.java
@@ -1,14 +1,41 @@
 package com.concurrente.server.server;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private TextField portTextField;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        numericOnly(portTextField);
+    }
+    public static void numericOnly(final TextField field) {
+        field.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(
+                    ObservableValue<? extends String> observable,
+                    String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    field.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+    }
+
 }
